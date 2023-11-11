a39e52b69357d3c4c3efc6edc633b8f8e70adebe
Paulo Alvarez <paulo.manuel.alvarez@gmail.com>
2023-11-11 15:20:48 -0300

    ultima version

--- app/controllers/user.api.controller.php
+++ app/controllers/user.api.controller.php
@@ -12,42 +12,8 @@
             $this->authHelper = new AuthHelper();
             $this->model = new UserModel();
         }
-        /*function getToken($params = []) {
-            $basic = $this->authHelper->getAuthHeaders(); // Darnos el header 'Authorization:' 'Basic: base64(usr:pass)'
-
-            if(empty($basic)) {
-                $this->view->response('No envió encabezados de autenticación.', 401);
-                return;
-            }
-
-            $basic = explode(" ", $basic); // ["Basic", "base64(usr:pass)"]
-
-            if($basic[0]!="Basic") {
-                $this->view->response('Los encabezados de autenticación son incorrectos.', 401);
-                return;
-            }
-
-            $userpass = base64_decode($basic[1]); // usr:pass
-            $userpass = explode(":", $userpass); // ["usr", "pass"]
-
-            $user = $userpass[0];
-            $pass = $userpass[1];
-
-            $userdata = [ "name" => $user, "id" => 123, "role" => 'ADMIN' ]; // Llamar a la DB
-
-            if($user == "Nico" && $pass == "web") {
-                // Usuario es válido
-                
-                $token = $this->authHelper->createToken($userdata);
-                $this->view->response($token);
-            } else {
-                $this->view->response('El usuario o contraseña son incorrectos.', 401);
-            }
-        }
-    }*/
-
-
-       function getToken($params = []) {
+        
+        function getToken($params = []) {
             $basic = $this->authHelper->getAuthHeaders(); 
 
             if(empty($basic)) {
